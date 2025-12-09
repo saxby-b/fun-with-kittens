517a3eccdb93aa70dab3a95045f307f487656e1b
C. Saxby Bain <csaxbybain@yahoo.com>
2025-12-09 15:19:38 -0500

    Added code to factory function

--- js/main.js
+++ js/main.js
@@ -6,6 +6,19 @@ const pet = {
     nap: function () {
     `${this.name} needs a nap!`;
     this.isTired = 1;
+    },
+    playTime: function () {
+        if(isTired = 10) {
+            console.log(`${this.name} is too sleepy to play.`);
+            this.nap();
+        return;
+        }
+        else {
+            console.log(`${name} loves to play!`);
+            this.isTired +1;
+        }
+
     }
-}
+};
+return;
 }
